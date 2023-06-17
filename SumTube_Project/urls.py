14bca22688ab2a4c1080a5c9e1e80e326d2122f5
gdsakelaris <gsakelar@depaul.edu>
2023-06-17 04:46:31 -0500

    R9

--- SumTube_Project/urls.py
+++ SumTube_Project/urls.py
@@ -7,8 +7,8 @@ app_name = 'SumTube_Project'
 urlpatterns = [
     path("", views.index),
     path('admin/', admin.site.urls),
-    # re_path('add_transcript/', views.add_transcript, name='add_transcript'),
     re_path('results/', views.add_transcript, name='results'),
+    # re_path('add_transcript/', views.add_transcript, name='add_transcript'),
     # re_path('contact/', views.contact, name='contact'),
     # re_path('post_ticket/', views.post_ticket, name='post_ticket'),
     # re_path('get_contact/', views.get_contact, name='get_contact'),
