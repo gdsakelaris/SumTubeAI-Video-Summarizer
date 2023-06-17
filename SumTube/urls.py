d9f0f7ce6ade85082ffe1292c252dea168870e34
gdsakelaris <gsakelar@depaul.edu>
2023-06-17 03:19:10 -0500

    R6

--- SumTube/urls.py
+++ SumTube/urls.py
@@ -14,16 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth.views import LoginView
 
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include  # error was being thrown w/o this
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/login/', LoginView.as_view(), name='login'),
-    path("", include('SumTube_Project.urls')),
-    # other URL patterns for your project
-    # ...
     path('accounts/', include('django.contrib.auth.urls')),
+    path("SumTube_Project/", include('SumTube_Project.urls')),
+    path('admin/', admin.site.urls),
 ]
--- SumTube_Project/urls.py
+++ SumTube_Project/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'SumTube_Project'
@@ -6,7 +6,7 @@ app_name = 'SumTube_Project'
 urlpatterns = [
     path("", views.index),
     # re_path('add_transcript/', views.add_transcript, name='add_transcript'),
-    #re_path('results/', views.results, name='results'),
+    re_path('results/', views.add_transcript, name='results'),
     # re_path('contact/', views.contact, name='contact'),
     # re_path('post_ticket/', views.post_ticket, name='post_ticket'),
     # re_path('get_contact/', views.get_contact, name='get_contact'),
